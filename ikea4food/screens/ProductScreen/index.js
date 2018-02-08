2b8e7203c9150dba8802d0bedf5f47d31e4745c1
Ze Xuan <syltaris@gmail.com>
2018-02-09 00:56:55 +0800

    added method to fetch all products from db to productpage

--- ikea4food/screens/ProductScreen/index.js
+++ ikea4food/screens/ProductScreen/index.js
@@ -8,6 +8,8 @@ import {
     Card
 } from 'react-native-elements';
 
+import DataHelpers from '../../models/schemas';
+
 import MainHeader from '../../components/MainHeader';
 import BottomNav from '../../components/BottomNav';
 import styles from '../../stylesheets/styles';
@@ -15,45 +17,18 @@ import styles from '../../stylesheets/styles';
 export default class ProductScreen extends Component {
     constructor() {
         super();
+
+        let products = DataHelpers.getAllProducts();
         this.state = {
-            products: [
-                {
-                    key: 1,
-                    name: 'McCreamy',
-                    image_uri: {uri: 'https://source.unsplash.com/collection/345760/800x100'},
-                    description: 'This is a lovely package of greens.',
-                    price: '$4.99'
-                },
-                {
-                    key: 2,
-                    name: 'McCreamier',
-                    image_uri: {uri: 'https://source.unsplash.com/collection/345760/800x101'},
-                    description: 'This is a lovely package of greens.',
-                    price: '$4.99'
-                },
-                {
-                    key: 3,
-                    name: 'McCreamiest',
-                    image_uri: {uri: 'https://source.unsplash.com/collection/345760/801x100'},
-                    description: 'This is a lovely package of greens.',
-                    price: '$4.99'
-                },
-                {
-                    key: 4,
-                    name: 'Cocaine',
-                    image_uri: {uri: 'https://source.unsplash.com/collection/345760/801x101'},
-                    description: 'This is a lovely package of greens.',
-                    price: '$4.99'
-                }
-            ]
+            products: products
         }
     }
 
     _renderProductCard = ({item, seperator}) => {
         return (
             <Card
-            key={item.key} 
-            image={item.image_uri}
+            key={item.id}
+            image={{uri: item.image_uri}}
             featuredSubtitle={item.name}
             containerStyle={{width: 400}}>
                 <Text>{item.description}</Text>
@@ -73,6 +48,7 @@ export default class ProductScreen extends Component {
                         <FlatList
                         showsVerticalScrollIndicator={false}
                         data={this.state.products}
+                        keyExtractor={(item) => item.id}
                         renderItem={this._renderProductCard} />
                     </View>
                     <BottomNav navigation={this.props.navigation} />
