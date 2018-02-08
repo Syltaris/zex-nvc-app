c8b539573699575d8807c265c397cc1826292f49
Ze Xuan <syltaris@gmail.com>
2018-02-09 00:56:38 +0800

    fix schema errors; added fixtures

--- ikea4food/models/schemas.js
+++ ikea4food/models/schemas.js
@@ -3,50 +3,24 @@ import Realm from 'realm';
 import UserModel from './User';
 import ProductModel from './Product';
 
-const UserSchema = {
-    name: 'User',
+const AddressSchema = {
+    name: 'Address',
     properties: {
-        key: 'string',
-        profile: {
-            name: 'string',
-            age: 'int',
-            email: 'string',
-            contact_number: 'string',
-            address: {
-                street: 'string',
-                state: 'string',
-                zip: 'string'
-            }
-        },
-    },
-    subscriptions: 'Package[]',
-    shoppingCart: 'Product[]'
-}
-const PackageSchema = {
-    name: 'Package',
-    properties: {
-        product: 'Product',
-        next_arrival_time: 'date'
+        street: 'string',
+        state: 'string',
+        zip: 'string'
     }
 }
-const ProductSchema = {
-    name: 'Product',
+const ProfileSchema = {
+    name: 'Profile',
+    primaryKey: 'id',
     properties: {
-        key: 'string',
+        id: 'string',
         name: 'string',
-        image_uri: 'string',
-        description: 'string',
-        price: 'string',
-        product_composition: 'Food{}'
-    }
-}
-const FoodSchema = {
-    name: 'Food',
-    properties: {
-        ingredients: 'Ingredient[]'
-    },
-    recipe: {
-        instructions: 'string[]'
+        age: 'int',
+        email: 'string',
+        contact_number: 'string',
+        address: 'Address'
     }
 }
 const IngredientSchema = {
@@ -58,9 +32,60 @@ const IngredientSchema = {
         amt_to_price_ratio: 'float',
     }
 }
+const InstructionsSchema = {
+    name: 'Instructions',
+    properties: {
+        instructions: 'string[]'
+    }
+}
+const FoodSchema = {
+    name: 'Food',
+    properties: {
+        ingredients: 'Ingredient[]',
+        recipe: 'Instructions'
+    },
+
+}
+const ProductSchema = {
+    name: 'Product',
+    primaryKey: 'id',
+    properties: {
+        id: 'string',
+        name: 'string',
+        image_uri: 'string',
+        description: 'string',
+        price: 'string',
+        product_composition: 'Food?'
+    }
+}
+const PackageSchema = {
+    name: 'Package',
+    properties: {
+        product: 'Product',
+        next_arrival_time: 'date'
+    }
+}
+const UserSchema = {
+    name: 'User',
+    properties: {
+        key: 'string',
+        profile: 'Profile',
+        subscriptions: 'Package[]',
+        shoppingCart: 'Product[]'
+    },
+}
 
 let database = new Realm({
-    schema: [UserSchema, PackageSchema, ProductSchema, FoodSchema, IngredientSchema]
+    schema: [
+        AddressSchema,
+        ProfileSchema,
+        IngredientSchema,
+        InstructionsSchema,
+        FoodSchema, 
+        ProductSchema,
+        PackageSchema, 
+        UserSchema,
+    ]
 });
 
 export default DataHelpers = {
@@ -69,5 +94,70 @@ export default DataHelpers = {
     },
     getUserData() {
         return database.objects('User');
+    },
+
+    insertUser() {
+        if (database.objects('User').filtered("title = '" + todo.title + "'").length) return;
+
+    },
+    insertProduct(product) {
+        if(database.objects('Product').filtered("name = '" + product.name + "'").length) return;
+
+        database.write(() => {
+            database.create('Product', product);
+        })
     }
 }
+
+//populate Products
+DataHelpers.insertProduct(new ProductModel('McCreamy', 
+'https://source.unsplash.com/collection/345760/800x100', 
+'This is a lovely package of greens.', 
+'$4.99',
+{}));
+DataHelpers.insertProduct(new ProductModel('McCreamier', 
+'https://source.unsplash.com/collection/345760/800x101', 
+'This is a lovelier package of greens.', 
+'$14.99',
+{}));
+DataHelpers.insertProduct(new ProductModel('McCreamiest', 
+'https://source.unsplash.com/collection/345760/801x100', 
+'This is the loveliest package of greens.', 
+'$34.99',
+{}));
+DataHelpers.insertProduct(new ProductModel('McCocaine', 
+'https://source.unsplash.com/collection/345760/801x101', 
+'This is a ... nose candy.', 
+'$44.99',
+{}));
+
+[
+    {
+        key: 1,
+        name: 'McCreamy',
+        image_uri: {uri: 'https://source.unsplash.com/collection/345760/800x100'},
+        description: 'This is a lovely package of greens.',
+        price: '$4.99'
+    },
+    {
+        key: 2,
+        name: 'McCreamier',
+        image_uri: {uri: 'https://source.unsplash.com/collection/345760/800x101'},
+        description: 'This is a lovely package of greens.',
+        price: '$4.99'
+    },
+    {
+        key: 3,
+        name: 'McCreamiest',
+        image_uri: {uri: 'https://source.unsplash.com/collection/345760/801x100'},
+        description: 'This is a lovely package of greens.',
+        price: '$4.99'
+    },
+    {
+        key: 4,
+        name: 'Cocaine',
+        image_uri: {uri: 'https://source.unsplash.com/collection/345760/801x101'},
+        description: 'This is a lovely package of greens.',
+        price: '$4.99'
+    }
+]
